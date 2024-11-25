150ddb27c263914417a869304bf9698c8075a9e3
anatoma08 <a23robtomtom@inspedralbes.cat>
2024-11-25 22:37:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ana_toma_uf1_act14/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ana_toma_uf1_act14
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
